d14c74159f0f630651ee13d4fe10a4ba7fff9bc1
Baharsah <me@baharsah.my.id>
2023-10-09 08:46:22 +0700

    stub

---
+++ src/lib/Livepay/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Livepay ; 
+
+
+class Config {
+
+    /** @var string $plugin Plugin Class definitor ; if null, using dummy.
+     * 
+     * @static
+     */
+    public static $plugin = null;
+
+
+
+
+}
